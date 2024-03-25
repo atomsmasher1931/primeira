d4bc73561ca7ebdde63ece5cef60f318c2b366be
Sergey Efimov <atomsmasger1931@gmail.com>
2024-03-25 09:30:06 +0300

    START, инициализация проекта

---
+++ src/Controller/ProbeController.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Проверка проекта на работоспособность
+ */
+class ProbeController extends AbstractController
+{
+	private const RESULT_APP = ['app' => 'ok'];
+
+	public function __construct(private readonly EntityManagerInterface $entityManager)
+	{
+	}
+
+	public function liveness(): Response
+	{
+		return $this->json(self::RESULT_APP);
+	}
+
+	public function readyness(): Response
+	{
+		$result = self::RESULT_APP;
+		try {
+			$queryBuilder = $this->entityManager->getConnection()->createQueryBuilder();
+			$queryBuilder->select('1');
+			$answer = $queryBuilder->executeQuery()->fetchFirstColumn();
+
+			$result['db'] = ['status' => 'ok', 'result' => $answer];
+		} catch (\Throwable $exception) {
+			$result['db'] = ['status' => 'fail', 'error' => $exception->getMessage()];
+		}
+
+		return $this->json($result);
+	}
+}
